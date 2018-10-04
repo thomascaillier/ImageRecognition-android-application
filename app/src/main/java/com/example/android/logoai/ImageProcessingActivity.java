2d69fb0458c620d82a6d672a88e3f2feddc3cbb7
thomascaillier <thomascaillier@gmail.com>
2018-10-04 11:57:11 +0200

    Add image posting on server Correct image getting from server

--- app/src/main/java/com/example/android/logoai/ImageProcessingActivity.java
+++ app/src/main/java/com/example/android/logoai/ImageProcessingActivity.java
@@ -6,26 +6,35 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ImageProcessingActivity extends AppCompatActivity {
 
-    private static String serverUrl = "http://www.google.com";
+    private static String serverUrl = "http://192.168.1.37:8080/imagerecognition";
 
     private int processingStep;
-    private static String[] steps = new String[]{
+    private static String[] stepsLabels = new String[]{
             "Envoi de l'image vers le serveur",
             "Détection des zones d'intérêt",
-            "Reconnaissance d'informations sur la zone d'intérêt"
+            "Reconnaissance d'informations sur la zone d'intérêt",
+            "Information reçue"
     };
 
     private VolleyError lastError = null;
@@ -33,9 +42,6 @@ public class ImageProcessingActivity extends AppCompatActivity {
     private RequestQueue requestQueue;
 
     private Bitmap image;
-    private int maxWidth;
-    private int maxHeight;
-    private Bitmap.Config bitmapConfig;
 
     public ImageView imageView;
     public TextView textView;
@@ -61,47 +67,103 @@ public class ImageProcessingActivity extends AppCompatActivity {
             throw new IllegalArgumentException();
         }
 
-        maxWidth = image.getWidth();
-        maxHeight = image.getHeight();
-        bitmapConfig = image.getConfig();
-
         imageView = findViewById(R.id.imageProcessing);
         textView = findViewById(R.id.consoleTextView);
 
         processingStep = 0;
         requestQueue = Volley.newRequestQueue(this);
 
-        requestStep();
+        updateView();
+        postImage();
+
+    }
+
+    private void addLine(String message) {
+        textView.append(System.getProperty("line.separator"));
+        textView.append(message);
 
     }
 
     private void updateView() {
         imageView.setImageBitmap(image);
-        textView.append(System.getProperty("line.separator"));
         if (lastError == null)
-            textView.append(steps[processingStep]);
+            addLine(stepsLabels[processingStep]);
         else {
-            textView.append("Erreur : " + lastError.getMessage());
+            addLine("Erreur : " + lastError.getMessage());
         }
     }
 
-    private void requestStep() {
-        ImageRequest request = new ImageRequest(
+    public String getEncodedImage() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        return Base64.encodeToString(imageBytes, Base64.URL_SAFE);
+    }
+
+    public void setDecodedImage(String data) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String base64Encoded = Base64.encodeToString(imageBytes, Base64.URL_SAFE);
+    }
+
+    private void postImage() {
+        StringRequest postRequest = new StringRequest(
+                Request.Method.POST,
                 serverUrl,
-                new Response.Listener<Bitmap>() {
+                new Response.Listener<String>() {
                     @Override
-                    public void onResponse(Bitmap response) {
-                        image = response;
+                    public void onResponse(String response) {
                         processingStep++;
                         updateView();
-                        if(processingStep < steps.length)
+                        if (processingStep < stepsLabels.length)
                             requestStep();
                     }
                 },
-                maxWidth,
-                maxHeight,
-                ImageView.ScaleType.CENTER,
-                bitmapConfig,
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        lastError = error;
+                        updateView();
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("image", getEncodedImage());
+                System.out.println(getEncodedImage());
+                return params;
+            }
+        };
+        requestQueue.add(postRequest);
+    }
+
+    private void requestStep() {
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.GET,
+                serverUrl + "?step=" + Integer.toString(processingStep),
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            if (Boolean.valueOf((String) response.get("Success"))) {
+                                setDecodedImage((String) response.get("Image"));
+                            }
+                            addLine((String) response.get("Message"));
+                            processingStep++;
+                            updateView();
+                            if (processingStep < stepsLabels.length - 1)
+                                requestStep();
+                            else {
+                                addLine((String) response.get("Url"));
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
